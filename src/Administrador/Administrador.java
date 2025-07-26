349a2b426b69dda81364335ad12cb1df4201e1b2
Nayely Ayol <nayely.ayol@epn.edu.ec>
2025-07-25 21:36:56 -0500

    Navegacion de paneles

--- src/Administrador/Administrador.java
+++ src/Administrador/Administrador.java
@@ -3,6 +3,8 @@ package Administrador;
 import javax.swing.*;
 import javax.swing.border.LineBorder;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Administrador extends JFrame{
     private JPanel Principal;
@@ -11,28 +13,34 @@ public class Administrador extends JFrame{
     private JPanel alertPen;
     private JPanel eficienciaPro;
     private JTextArea rutastextArea1;
-    private JButton dashboardButton;
-    private JButton gestiónDeRutasButton;
-    private JButton estudiantesButton;
-    private JButton conductoresButton;
-    private JButton reportesButton;
-    private JButton alertasButton;
+    private JPanel Cards;
+    private JButton gestionDeRutasButton;
+    private JPanel Dashboard;
     private JTextField textField1;
     private JTextField textField2;
     private JTextField textField3;
     private JTextField textField4;
     private JTextField textField5;
     private JComboBox comboBox1;
-    private JPanel Cards;
-    private JPanel Resgistrar;
+    private JPanel Registrar_rutas;
+    private JPanel Barra_navegacion;
+    private JButton dashboardButton;
+    private JButton estudiantesButton;
+    private JButton conductoresButton;
+    private JButton reportesButton;
+    private JButton alertasButton;
 
     public Administrador(){
-        setContentPane(Cards);
+        setContentPane(Principal);
         setTitle("Administrador");
         setExtendedState(MAXIMIZED_BOTH);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
         setVisible(true);
+
+        Cards.setLayout(new CardLayout());
+        Cards.add(Dashboard, "dashboard");
+        Cards.add(Registrar_rutas, "rutas");
 
         rutasAct.setBorder(new LineBorder(new Color(206,255,253), 2));
         estuReg.setBorder(new LineBorder(new Color(206,255,253), 2));
@@ -40,11 +48,16 @@ public class Administrador extends JFrame{
         eficienciaPro.setBorder(new LineBorder(new Color(206,255,253), 2));
         rutastextArea1.setBorder(new LineBorder(new Color(206,255,253), 2));
 
-
+        gestionDeRutasButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                mostrarCarta("rutas");
+            }
+        });
     }
 
     public void mostrarCarta(String nombreCarta) {
         CardLayout cl = (CardLayout) Cards.getLayout();
-        cl.show(Cards, String.valueOf(Principal));
+        cl.show(Cards, nombreCarta);
     }
 }
