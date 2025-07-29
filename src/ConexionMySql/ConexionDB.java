3d4ebdfac56ef0671aafe0a06db451c637b219e5
Emy <emily.galeas@epn.edu.ec>
2025-07-28 20:31:55 -0500

    cambio de url, usuario y clave

--- src/ConexionMySql/ConexionDB.java
+++ src/ConexionMySql/ConexionDB.java
@@ -7,9 +7,9 @@ import java.sql.SQLException;
 public class ConexionDB {
 
     // declaramos constantes para los parámetros de conexión: URL, usuario y contraseña
-    private static final String url = "jdbc:mysql://uerkn6um7tcezrku:iK0joXvqIaXk9MgMpxEV@bdidgpcn61qhjmtojryx-mysql.services.clever-cloud.com:3306/bdidgpcn61qhjmtojryx";
-    private static final String usuario = "uerkn6um7tcezrku";
-    private static final String clave = "iK0joXvqIaXk9MgMpxEV";
+    private static final String url = "jdbc:mysql://uuta6qoqasndu51a:6Mkwjg8zwGZddbo0uwIm@bye3meozep3t8sfrhixc-mysql.services.clever-cloud.com:3306/bye3meozep3t8sfrhixc";
+    private static final String usuario = "uuta6qoqasndu51a";
+    private static final String clave = "6Mkwjg8zwGZddbo0uwIm";
 
     public static Connection getConnection(){
         try {
